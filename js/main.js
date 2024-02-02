b77874f90080e35068f1ce02972d34eaa16925ea
Indranil Halder <indranilhalder.dev@gmail.com>
2024-02-03 02:01:40 +0530

    Car physics implemented

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
+import Car from "./car";
 
+const canvas = document.getElementById("myCanvas");
+
+canvas.width = 200;
+
+const ctx = canvas.getContext("2d");
+
+const car = new Car(100, 100, 30, 50);
+car.draw(ctx);
+
+function animate() {
+  car.update();
+  canvas.height = window.innerHeight;
+  car.draw(ctx);
+  requestAnimationFrame(animate);
+}
+
+animate();
