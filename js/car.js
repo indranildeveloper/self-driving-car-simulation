de94a94453e1a5220840f32f932158c6555cd4f7
Indranil Halder <indranilhalder.dev@gmail.com>
2024-02-03 23:30:38 +0530

    Sensors

--- js/car.js
+++ js/car.js
@@ -57,9 +57,9 @@ class Car {
     this.y -= Math.cos(this.angle) * this.speed;
   }
 
-  update() {
+  update(roadBorders) {
     this.#move();
-    this.sensor.update();
+    this.sensor.update(roadBorders);
   }
 
   draw(ctx) {
