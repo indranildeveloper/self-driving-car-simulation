6c81e0bdff59588f82fa885be009aece806de24f
Indranil Halder <indranilhalder.dev@gmail.com>
2024-02-04 14:40:42 +0530

    Visualization of neural network

--- js/main.js
+++ js/main.js
@@ -1,35 +1,46 @@
 import Car from "./car";
 import Road from "./road";
+import Visualizer from "./visualizer";
 
-const canvas = document.getElementById("myCanvas");
-canvas.width = 200;
-const ctx = canvas.getContext("2d");
+const carCanvas = document.getElementById("carCanvas");
+const networkCanvas = document.getElementById("networkCanvas");
 
-const road = new Road(canvas.width / 2, canvas.width * 0.9);
+carCanvas.width = 200;
+networkCanvas.width = 400;
+
+const carCtx = carCanvas.getContext("2d");
+const networkCtx = networkCanvas.getContext("2d");
+
+const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
 const car = new Car(road.getLaneCenter(1), 100, 30, 50, "AI");
 
 const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2)];
 
-function animate() {
+function animate(time) {
   for (let i = 0; i < traffic.length; i++) {
     traffic[i].update(road.borders, []);
   }
 
   car.update(road.borders, traffic);
-  canvas.height = window.innerHeight;
 
-  ctx.save();
-  ctx.translate(0, -car.y + canvas.height * 0.7);
+  carCanvas.height = window.innerHeight;
+  networkCanvas.height = window.innerHeight;
 
-  road.draw(ctx);
+  carCtx.save();
+  carCtx.translate(0, -car.y + carCanvas.height * 0.7);
+
+  road.draw(carCtx);
 
   for (let i = 0; i < traffic.length; i++) {
-    traffic[i].draw(ctx, "red");
+    traffic[i].draw(carCtx, "red");
   }
 
-  car.draw(ctx, "blue");
+  car.draw(carCtx, "blue");
 
-  ctx.restore();
+  carCtx.restore();
+
+  networkCtx.lineDashOffset = -time / 50;
+  Visualizer.drawNetwork(networkCtx, car.brain);
   requestAnimationFrame(animate);
 }
 
